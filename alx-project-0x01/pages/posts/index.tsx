b6cc23b33bdb3bbcb13c2ae73b0943f17df42486
Omondi Danish <omondidanish77@gmail.com>
2025-09-21 23:35:13 +0300

    Update index.tsx

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -2,7 +2,7 @@ import PostCard from "@/components/common/PostCard";
 import PostModal from "@/components/common/PostModal";
 import Header from "@/components/layout/Header";
 import { PostData, PostProps, PostsPageProps } from "@/interfaces";
-import {PostProps};
+import { PostProps };
 import { useState } from "react";
 
 const Posts: React.FC<PostsPageProps> = ({ posts }) => {
