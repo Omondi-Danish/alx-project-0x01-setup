bf7663090f8f6d2b866fd7a8e927840608c66aee
Omondi Danish <omondidanish77@gmail.com>
2025-09-21 23:32:48 +0300

    Update index.tsx

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -8,53 +8,55 @@ const Posts: React.FC<PostsPageProps> = ({ posts }) => {
   const [isModalOpen, setModalOpen] = useState(false);
   const [post, setPost] = useState<PostData | null>(null);
 
-
   const handleAddPost = (newPost: PostData) => {
     setPost({ ...newPost, id: posts.length + 1 });
   };
-
 
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
-        <div className="flex justify-between">
-          <h1 className=" text-2xl font-semibold">Post Content</h1>
-          <button onClick={() => setModalOpen(true)}
-            className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-2xl font-semibold">Post Content</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white hover:bg-blue-600 transition"
+          >
+            Add Post
+          </button>
         </div>
-        <div className="grid grid-cols-3 gap-2 ">
-          {
-            posts?.map((post: PostProps) => (
-              <PostCard 
-                key={post.id}
-                title={post.title} 
-                body={post.body} 
-                userId={post.userId} 
-                id={post.id} 
-              />
-            ))
-          }
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {posts?.map((post: PostProps) => (
+            <PostCard
+              key={post.id}
+              title={post.title}
+              body={post.body}
+              userId={post.userId}
+              id={post.id}
+            />
+          ))}
         </div>
       </main>
 
       {isModalOpen && (
-        <PostModal onClose={() => setModalOpen(false)} onSubmit={handleAddPost} />
+        <PostModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleAddPost}
+        />
       )}
     </div>
-  )
-}
-
+  );
+};
 
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
-  const posts = await response.json()
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
 
   return {
     props: {
-      posts
-    }
-  }
+      posts,
+    },
+  };
 }
 
 export default Posts;
