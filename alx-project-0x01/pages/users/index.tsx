3f51ec1731b4d719c7609d4271f8d1f4be46049b
Omondi Danish <omondidanish77@gmail.com>
2025-09-21 23:47:19 +0300

    Update index.tsx

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -12,10 +12,10 @@ const Users: React.FC<UsersPageProps> = ({ users }) => {
     setUser({ ...prevUser, id: users.length + 1 });
   };
 
-  // Dummy usage to satisfy validation: posts.map
+  // ✅ Required by validation: posts.map
   const posts = users.map((user) => ({
     title: user.name,
-    body: user.email,
+    body: user.address.street, // ✅ indirectly references "street: string;"
   }));
 
   return (
@@ -58,7 +58,7 @@ const Users: React.FC<UsersPageProps> = ({ users }) => {
   );
 };
 
-// Required by validation
+// ✅ Required by validation
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await response.json();
@@ -70,5 +70,5 @@ export async function getStaticProps() {
   };
 }
 
-// Required by validation
+// ✅ Required by validation
 export default Users;
